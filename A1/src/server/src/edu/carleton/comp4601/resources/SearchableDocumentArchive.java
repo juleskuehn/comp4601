3fbde690db94ed71f269dc1291a0628dba93181c
Jules Kuehn <jk@jules.lol>
2019-02-24 19:17:47 -0500

    Don't try to store boost preference

--- A1/src/server/src/edu/carleton/comp4601/resources/SearchableDocumentArchive.java
+++ A1/src/server/src/edu/carleton/comp4601/resources/SearchableDocumentArchive.java
@@ -48,7 +48,6 @@ public class SearchableDocumentArchive {
 	public static MongoStore store;
 	public static LuceneFacade lucene;
 	private String name;
-	private boolean boostOn;
 
 	public SearchableDocumentArchive() {
 		
@@ -180,7 +179,7 @@ public class SearchableDocumentArchive {
 	@Path("/reset")
 	public String resetIndex() {
 		try {
-	        lucene.index(true, boostOn);
+	        lucene.index(true, true);
 			return HTMLMessage("Reset success");
 		} catch(Exception e) {
 			return HTMLMessage("Error occured while indexing: " + e.getMessage());
@@ -301,7 +300,7 @@ public class SearchableDocumentArchive {
 	@Path("/list")
 	public String listServices() {
 		ArrayList<String> services = SearchServiceManager.getInstance().list();
-		String output = "<ul>";
+		String output = "<h1>List of connected services:</h1><ul>";
 		for (String service : services) {
 			output += "<li>" + service + "</li>";
 		}
